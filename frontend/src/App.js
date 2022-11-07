60e6a8a50440ac34571504ecaa34b1f17db982ec
yz96 <68858113+yangzho12138@users.noreply.github.com>
2022-11-06 19:11:13 -0600

    register

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,6 +6,7 @@ import HomeScreen from './screens/HomeScreen';
 import ProductScreen from './screens/ProductScreen';
 import CartScreen from './screens/CartScreen';
 import LoginScreen from './screens/LoginScreen';
+import RegisterScreen from './screens/RegisterScreen';
 import { Routes, Route } from 'react-router-dom'
 
 const App = () => {
@@ -17,6 +18,7 @@ const App = () => {
             <Routes>
               <Route path='/' element={<HomeScreen />}/>
               <Route path='/login' element={<LoginScreen />} />
+              <Route path='/register' element={<RegisterScreen />} />
               <Route path='/product/:id' element={<ProductScreen />} />
               <Route path='/cart/:id' element={<CartScreen />} />
               <Route path='/cart' element={<CartScreen />} />
---
+++ frontend/src/screens/RegisterScreen.jsx
@@ -0,0 +1,81 @@
+import React, { useState, useEffect } from 'react'
+import { Link, useLocation, useNavigate } from 'react-router-dom'
+import { Form, Button, Row, Col } from 'react-bootstrap'
+import { useDispatch, useSelector } from 'react-redux';
+import Message from '../components/Message';
+import Loader from '../components/Loader';
+import FormContainer from '../components/FormContainer';
+import { register } from '../actions/userActions'
+
+const RegisterScreen = () => {
+    const [email, setEmail] = useState('');
+    const [name, setName] = useState('');
+    const [password, setPassword] = useState('');
+    const [confirmPassword, setConfirmPassword] = useState('');
+    const [message, setMessage] = useState(null);
+
+    const location = useLocation();
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+
+    const userRegister = useSelector(state => state.userRegister) 
+    const { loading, error, userInfo } = userRegister; 
+
+
+    const redirect = location.search ? location.search.split('=')[1] : '/'
+
+    useEffect(() => {
+      // if user has already login
+      if(userInfo){
+          navigate(redirect);
+      }
+    }, [navigate, redirect, userInfo])
+
+    const submitHandler = (e) => {
+      e.preventDefault() // nor refresh
+      if(password !== confirmPassword){
+        setMessage('Password do not match')
+      }else{
+        dispatch(register(name, email, password))
+      }
+    }
+
+  return (
+    <FormContainer>
+      <h1>Sign Up</h1>
+      {error && <Message variant='danger'>{error}</Message>}
+      {message && <Message variant='danger'>{message}</Message>}
+      {loading && <Loader />}
+      <Form onSubmit={submitHandler}>
+        <Form.Group controlId='name'>
+            <Form.Label>Name</Form.Label>
+            <Form.Control type='name' placeholder='Enter Name' value={name} onChange={(e) => setName(e.target.value)}></Form.Control>
+        </Form.Group>
+        <Form.Group controlId='email'>
+            <Form.Label>Email Address</Form.Label>
+            <Form.Control type='email' placeholder='Enter Email' value={email} onChange={(e) => setEmail(e.target.value)}></Form.Control>
+        </Form.Group>
+        <Form.Group controlId='password'>
+            <Form.Label>Password</Form.Label>
+            <Form.Control type='password' placeholder='Enter Password' value={password} onChange={(e) => setPassword(e.target.value)}></Form.Control>
+        </Form.Group>
+        <Form.Group controlId='confirmPassword'>
+            <Form.Label>Confirm Password</Form.Label>
+            <Form.Control type='password' placeholder='Confirm Password' value={confirmPassword} onChange={(e) => setConfirmPassword(e.target.value)}></Form.Control>
+        </Form.Group>
+        <Button className='my-3' type='submit' variant='primary'>
+          Register
+        </Button>
+      </Form>
+
+      <Row className='py-3'>
+        <Col>
+          Have an Account? 
+          <Link className='mx-3' to={redirect ? `/login?redirect=${redirect}` : '/login'}>Login</Link>
+        </Col>
+      </Row>
+    </FormContainer>
+  )
+}
+
+export default RegisterScreen
