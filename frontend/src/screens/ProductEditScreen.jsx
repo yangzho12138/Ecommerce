7fd60960a489574a7668506a18cd2d941a1c7c64
yz96 <68858113+yangzho12138@users.noreply.github.com>
2022-12-05 10:42:18 -0600

    peoduct edit (bug)

--- frontend/src/screens/ProductEditScreen.jsx
+++ frontend/src/screens/ProductEditScreen.jsx
@@ -5,7 +5,8 @@ import { useDispatch, useSelector } from 'react-redux';
 import Message from '../components/Message';
 import Loader from '../components/Loader';
 import FormContainer from '../components/FormContainer';
-import { listProductDetails } from '../actions/productActions'
+import { listProductDetails, updateProduct } from '../actions/productActions'
+import { PRODUCT_UPDATE_RESET } from '../constants/productConstants';
 
 const ProductEditScreen = () => {
     const params = useParams()
@@ -26,24 +27,40 @@ const ProductEditScreen = () => {
     const productDetails = useSelector(state => state.productDetails) 
     const { loading, error, product } = productDetails; 
 
+    const productUpdate = useSelector(state => state.productUpdate) 
+    const { loading: loadingUpdate, error: errorUpdate, success: successUpdate } = productUpdate; 
+
     useEffect(() => {
-      if(!product.name || product._id !== productId){
-        console.log(product, productId)
-        dispatch(listProductDetails(productId))
+      if(successUpdate){
+        dispatch({type: PRODUCT_UPDATE_RESET})
+        navigate('/admin/productlist')
       }else{
-        setName(product.name)
-        setPrice(product.price)
-        setImage(product.image)
-        setBrand(product.brand)
-        setCategory(product.category)
-        setCountInStock(product.countInStock)
-        setDescription(product.description)
+        if(!product.name || product._id !== productId){
+          dispatch(listProductDetails(productId))
+        }else{
+          setName(product.name)
+          setPrice(product.price)
+          setImage(product.image)
+          setBrand(product.brand)
+          setCategory(product.category)
+          setCountInStock(product.countInStock)
+          setDescription(product.description)
+        }
       }
-    }, [dispatch, productId, product])
+    }, [productId, product, successUpdate, dispatch, navigate])
 
     const submitHandler = (e) => {
       e.preventDefault() // nor refresh
-      
+      dispatch(updateProduct({
+        _id: productId,
+        name,
+        price,
+        image,
+        brand,
+        category,
+        description,
+        countInStock,
+      }))
     }
 
   return (
@@ -51,6 +68,8 @@ const ProductEditScreen = () => {
         <Link to='/admin/productList' className='btn btn-light my-3'>Go Back</Link>
         <FormContainer>
       <h1>Edit Product</h1>
+      {loadingUpdate && <Loader />}
+      {errorUpdate && <Message variant='danger'>{errorUpdate}</Message>}
       {loading ? <Loader /> : error ? <Message variant='danger'>{error}</Message> : (
         <Form onSubmit={submitHandler}>
             <Form.Group controlId='name' className='my-3'>
